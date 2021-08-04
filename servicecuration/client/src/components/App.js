7ffa9926e47ce86d782975ef583a58f24d4073a9
김호진 <gimhojin@gimhojin-ui-MacBookAir.local>
2021-08-04 20:48:10 +0900

    sc

---
+++ servicecuration/client/src/components/App.js
@@ -0,0 +1,34 @@
+import React, { Suspense } from 'react';
+import {HashRouter as Router, Route} from "react-router-dom";
+// pages for this product
+import NavBar from "./views/NavBar/NavBar"
+import LandingPage from "./views/LandingPage/LandingPage.js"
+import UploadPage from "./views/UploadPage/UploadPage"
+import Footer from "./views/Footer/Footer"
+import LoginPage from "./views/UserPage/LoginPage"
+import RegisterPage from "./views/UserPage/RegisterPage"
+
+//null   Anyone Can go inside
+//true   only logged in user can go inside
+//false  logged in user can't go inside
+
+function App() {
+  return (
+    <Suspense fallback={(<div>Loading...</div>)}>
+        <Router>
+          <NavBar />
+      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+          <br/>
+          <br/>
+          <Route exact path="/" component={ LandingPage } />
+          <Route exact path="/uploads" component={ UploadPage } />
+          <Route exact path="/login" component={ LoginPage } />
+          <Route exact path="register" component={ RegisterPage } />
+      </div>
+        </Router>
+      <Footer />
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ servicecuration/client/src/components/tools/ResultCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card, Typography, Button } from "antd";
+
+const ResultCard = (props) => {
+
+    return (
+      <>
+      <span
+        style={{ width: 700, display:'flex', margin:10, border: '1px solid'}}
+      >
+        <span style={{display:'block', width:'30%', backgroundColor:'blue'}}>
+        <p> 토플 영단어 보카 </p>
+        <img style={{width:150}} src="https://play-lh.googleusercontent.com/FNXV9IWrS7n2VMq1R_bRqWXQw-n69fkSxovQ-Wt6BiW7S3T8UuYfymZ4hTXanrhyaaQ=s360-rw" />
+
+        <p>{props.number}</p>
+        </span>
+        <span style={{backgroundColor:'red', width:'70%'}}>
+        <Button style={{marginRight:'0'}}>서비스 비교하기</Button>
+        <p> 시간날 때 틈틈이 토플 단어를 외우는데 최적화 된 앱 </p>
+        <p> 100명 중 87명이 유용하다고 평가 </p>
+        <p> 100명 중 92명이 믿을만하다고 평가 </p>
+        <p> 추천 수 : {props.number} </p>
+        </span>
+      </span>
+      </>
+    )
+}
+
+export default ResultCard;
